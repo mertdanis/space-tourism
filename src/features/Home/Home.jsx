319df0539125a74840cb654d7097b53181678674
M. D <mertdanis@outlook.com>
2023-10-14 18:34:29 +0300

    update

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,31 +1,35 @@
 import { Link } from "react-router-dom";
 
+import Navbar from "../../components/Navbar";
+
 function Home() {
   return (
-    <div className="  text-white bg-cover flex flex-col mt-[160px]  ">
-      <div className=" flex items-center justify-center ">
-        <div className="flex flex-col    ">
-          <div className="flex flex-col  uppercase ">
-            <p className="tracking-[4.725px] text-h5 text-mainLight">
-              so, you want to travel to
+    <>
+      <div className="  text-white bg-cover flex flex-col mt-[160px]  ">
+        <div className=" flex items-center justify-center ">
+          <div className="flex flex-col    ">
+            <div className="flex flex-col  uppercase ">
+              <p className="tracking-[4.725px] text-h5 text-mainLight">
+                so, you want to travel to
+              </p>
+              <h1 className="text-h1  my-[80px]  ">space</h1>
+            </div>
+            <p className="w-1/2 text-body_text text-mainLight">
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
             </p>
-            <h1 className="text-h1  my-[80px]  ">space</h1>
           </div>
-          <p className="w-1/2 text-body_text text-mainLight">
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </div>
 
-        <Link to="/destinations">
-          <div className="  mt-[5rem] duration-300 text-mainDark  hover:bg-black hover:text-white   h-72 w-72 ml-auto  transition hover:-translate-y-2 rounded-full border cursor-pointer bg-white  flex items-center justify-center uppercase ">
-            <span className="p-6 text-h4  tracking-[2px]  ">Explore</span>
-          </div>
-        </Link>
+          <Link to="/destinations">
+            <div className="  mt-[5rem] duration-300 text-mainDark  hover:bg-black hover:text-white   h-72 w-72 ml-auto  transition hover:-translate-y-2 rounded-full border cursor-pointer bg-white  flex items-center justify-center uppercase ">
+              <span className="p-6 text-h4  tracking-[2px]  ">Explore</span>
+            </div>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/ui/Layout.jsx
+++ src/ui/Layout.jsx
@@ -1,10 +1,21 @@
 import Navbar from "../components/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 function Layout() {
+  const pageInfo = useLocation();
+
+  const currentPage = pageInfo.pathname;
+
+  console.log(currentPage);
+
   return (
     <div
-      className={`grid grid-cols-7 grid-rows-4 h-screen w-full bg-destinations  bg-cover text-white `}
+      className={`grid grid-cols-7 grid-rows-4 h-screen w-full bg-destinations  bg-cover text-white ${
+        currentPage === "/home" ? "bg-home" : ""
+      }
+      ${currentPage === "/crew" ? "bg-crew" : ""}
+      ${currentPage === "/technology" ? "bg-technology" : ""}
+      `}
     >
       <header className="col-span-full row-span-1	">
         <Navbar />
